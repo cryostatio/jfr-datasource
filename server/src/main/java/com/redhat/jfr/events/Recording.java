0dcbb477def9af3802ad555ca87ad107ab89c4c1
Jie Kang <jkang@redhat.com>
2019-05-07 16:34:27 -0400

    Update search to list type identifiers

--- server/src/main/java/com/redhat/jfr/events/Recording.java
+++ server/src/main/java/com/redhat/jfr/events/Recording.java
@@ -1,26 +1,67 @@
 package com.redhat.jfr.events;
 
+import java.io.File;
+import java.util.Iterator;
+
+import org.openjdk.jmc.common.item.IItem;
 import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.common.item.IItemIterable;
+import org.openjdk.jmc.common.item.IType;
+import org.openjdk.jmc.flightrecorder.JfrLoaderToolkit;
 
 public class Recording {
 
-    private String filename;
-    private IItemCollection events;
+  private String filename;
+  private IItemCollection events;
 
-    public Recording(String filename) {
-        this.filename = filename;
+  public Recording(String filename) {
+    this.filename = filename;
+    loadEvents();
+  }
+
+  public String search() {
+    if (events == null) {
+      return "[]";
     }
-
-    public String search() {
+    StringBuilder json = new StringBuilder();
+    json.append("[");
+    Iterator<IItemIterable> i = events.iterator();
+    while (i.hasNext()) {
+      try {
+        IItemIterable item = i.next();
+        IType<IItem> type = item.getType();
+        json.append("\"" + type.getIdentifier() + "\"");
+        json.append(",");
+      } catch (Exception e) {
         return "[]";
+      }
     }
 
-    public String query() {
-        return "[]";
-    }
+    json.deleteCharAt(json.length() - 1);
+    json.append("]");
+    return json.toString();
+  }
 
-    public String annotations() {
-        return "[]";
+  public String query() {
+    if (events == null) {
+      return "[]";
     }
+    return "[]";
+  }
+
+  public String annotations() {
+    if (events == null) {
+      return "[]";
+    }
+    return "[]";
+  }
+
+  private void loadEvents() {
+    try {
+      this.events = JfrLoaderToolkit.loadEvents(new File(this.filename));
+    } catch (Exception e) {
+
+    }
+  }
 
 }
