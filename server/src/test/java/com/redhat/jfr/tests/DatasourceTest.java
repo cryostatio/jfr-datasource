348e6742f7ad4cfd1e63e7bddb392b24d37bde24
Jie Kang <jkang@redhat.com>
2019-10-29 17:12:29 -0400

    Add more http tests for JFR and query endpoints

--- server/src/test/java/com/redhat/jfr/tests/DatasourceTest.java
+++ server/src/test/java/com/redhat/jfr/tests/DatasourceTest.java
@@ -2,15 +2,118 @@ package com.redhat.jfr.tests;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class DatasourceTest {
+
+  @AfterEach
+  public void afterEachDatasourceTest() {
+    File directory = new File("file-uploads");
+    if (directory.exists() && directory.isDirectory()) {
+      for (File f : directory.listFiles()) {
+        if (f.isFile()) {
+          f.delete();
+        }
+      }
+    }
+  }
+
+  @AfterAll
+  public static void afterAllDatasourceTest() {
+    File directory = new File("file-uploads");
+    if (directory.exists() && directory.isDirectory()) {
+      directory.delete();
+    }
+  }
+
   @Test
   public void testGet() throws Exception {
     given().when().get("/").then().statusCode(200).body(is(""));
   }
+
+  @Test
+  public void testPostUpload() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.1.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.1.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+  }
+
+  @Test
+  public void testPostSet() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.2.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.2.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+
+    expected = "Set: jmc.2.jfr" + System.lineSeparator();
+    given().body("jmc.2.jfr").when().post("/set").then().statusCode(200).body(is(expected));
+  }
+
+  @Test
+  public void testPostLoad() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.2.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.2.jfr" + System.lineSeparator() + "Set: jmc.2.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+  }
+
+  @Test
+  public void testGetList() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.1.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.1.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+
+    expected = "jmc.1.jfr" + System.lineSeparator();
+    given().when().get("/list").then().statusCode(200).body(is(expected));
+  }
+
+  @Test
+  public void testGetSearch() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.2.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.2.jfr" + System.lineSeparator() + "Set: jmc.2.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+
+    File outputFile = new File("src/test/resources/search.output.txt");
+    assertTrue(outputFile.exists());
+    expected = new String(Files.readAllBytes(outputFile.toPath()));
+    given().when().get("/search").then().statusCode(200).body(is(expected));
+  }
+
+  @Test
+  public void testPostQueryTimeseries() throws Exception {
+    File jfrFile = new File("src/test/resources/jmc.2.jfr");
+    assertTrue(jfrFile.exists());
+
+    String expected = "Uploaded: jmc.2.jfr" + System.lineSeparator() + "Set: jmc.2.jfr" + System.lineSeparator();
+    given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+
+    File inputFile = new File("src/test/resources/query.timeseries.input.txt");
+    assertTrue(inputFile.exists());
+    String input = new String(Files.readAllBytes(inputFile.toPath()));
+
+    File outputFile = new File("src/test/resources/query.timeseries.output.txt");
+
+    assertTrue(outputFile.exists());
+    expected = new String(Files.readAllBytes(outputFile.toPath()));
+    given().body(input).when().post("/query").then().statusCode(200).body(is(expected));
+  }
+
 }
