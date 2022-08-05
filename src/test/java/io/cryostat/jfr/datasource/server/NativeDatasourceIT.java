82e6151f4b83c3dd08ff94922911526bd8b12c6b
Thuan Vo <thvo@redhat.com>
2022-08-05 13:49:58 -0400

    test(native): add header checks in native image tests

--- src/test/java/io/cryostat/jfr/datasource/server/NativeDatasourceIT.java
+++ src/test/java/io/cryostat/jfr/datasource/server/NativeDatasourceIT.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Collections;
 
 import io.quarkus.test.junit.NativeImageTest;
 import org.junit.jupiter.api.AfterEach;
@@ -29,7 +30,7 @@ public class NativeDatasourceIT {
 
     @Test
     public void testGet() throws Exception {
-        given().when().get("/").then().statusCode(200).body(is(""));
+        given().when().get("/").then().statusCode(200).body(is("")).headers(Collections.emptyMap());
     }
 
     @Test
@@ -38,7 +39,16 @@ public class NativeDatasourceIT {
         assertTrue(jfrFile.exists());
 
         String expected = "Uploaded: jmc.cpu.jfr" + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/upload")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -47,10 +57,28 @@ public class NativeDatasourceIT {
         assertTrue(jfrFile.exists());
 
         String expected = "Uploaded: jmc.cpu.jfr" + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/upload")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         expected = "Set: jmc.cpu.jfr" + System.lineSeparator();
-        given().body("jmc.cpu.jfr").when().post("/set").then().statusCode(200).body(is(expected));
+        given().body("jmc.cpu.jfr")
+                .when()
+                .post("/set")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -63,7 +91,16 @@ public class NativeDatasourceIT {
                         + System.lineSeparator()
                         + "Set: jmc.cpu.jfr"
                         + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/load")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -72,16 +109,41 @@ public class NativeDatasourceIT {
         assertTrue(jfrFile.exists());
 
         String expected = "Uploaded: jmc.cpu.jfr" + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/upload")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         expected = "jmc.cpu.jfr" + System.lineSeparator();
-        given().when().get("/list").then().statusCode(200).body(is(expected));
+        given().when()
+                .get("/list")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("GET"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
     public void testGetListEmpty() throws Exception {
         String expected = "";
-        given().when().get("/list").then().statusCode(200).body(is(expected));
+        given().when()
+                .get("/list")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("GET"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -94,13 +156,30 @@ public class NativeDatasourceIT {
                         + System.lineSeparator()
                         + "Set: jmc.cpu.jfr"
                         + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/load")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         File outputFile = new File("src/test/resources/search.output.txt");
         assertTrue(outputFile.exists());
 
         expected = new String(Files.readAllBytes(outputFile.toPath()));
-        given().when().get("/search").then().statusCode(200).body(is(expected));
+        given().when()
+                .get("/search")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("application/json"))
+                .header("Access-Control-Allow-Methods", is("GET"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -113,7 +192,16 @@ public class NativeDatasourceIT {
                         + System.lineSeparator()
                         + "Set: jmc.cpu.jfr"
                         + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/load")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         File inputFile = new File("src/test/resources/query.timeseries.input.txt");
         assertTrue(inputFile.exists());
@@ -123,7 +211,16 @@ public class NativeDatasourceIT {
 
         assertTrue(outputFile.exists());
         expected = new String(Files.readAllBytes(outputFile.toPath()));
-        given().body(input).when().post("/query").then().statusCode(200).body(is(expected));
+        given().body(input)
+                .when()
+                .post("/query")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("application/json"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -136,7 +233,16 @@ public class NativeDatasourceIT {
                         + System.lineSeparator()
                         + "Set: jmc.cpu.jfr"
                         + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/load").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/load")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         File inputFile = new File("src/test/resources/query.table.input.txt");
         assertTrue(inputFile.exists());
@@ -146,7 +252,16 @@ public class NativeDatasourceIT {
 
         assertTrue(outputFile.exists());
         expected = new String(Files.readAllBytes(outputFile.toPath()));
-        given().body(input).when().post("/query").then().statusCode(200).body(is(expected));
+        given().body(input)
+                .when()
+                .post("/query")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("application/json"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -155,14 +270,41 @@ public class NativeDatasourceIT {
         assertTrue(jfrFile.exists());
 
         String expected = "Uploaded: jmc.cpu.jfr" + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/upload")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
-        given().body("jmc.cpu.jfr").when().delete("/delete").then().statusCode(204).body(is(""));
+        given().body("jmc.cpu.jfr")
+                .when()
+                .delete("/delete")
+                .then()
+                .statusCode(204)
+                .body(is(""))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("DELETE"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
     public void testDeleteFileNotExist() throws Exception {
-        given().body("jmc.cpu.jfr").when().delete("/delete").then().statusCode(404).body(is(""));
+        given().body("jmc.cpu.jfr")
+                .when()
+                .delete("/delete")
+                .then()
+                .statusCode(404)
+                .body(is(""))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("DELETE"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
@@ -171,11 +313,36 @@ public class NativeDatasourceIT {
         assertTrue(jfrFile.exists());
 
         String expected = "Uploaded: jmc.cpu.jfr" + System.lineSeparator();
-        given().multiPart(jfrFile).when().post("/upload").then().statusCode(200).body(is(expected));
+        given().multiPart(jfrFile)
+                .when()
+                .post("/upload")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("POST"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
 
         expected = "Deleted: jmc.cpu.jfr" + System.lineSeparator();
-        given().when().delete("/delete_all").then().statusCode(200).body(is(expected));
-        given().when().delete("/delete_all").then().statusCode(200).body(is(""));
+        given().when()
+                .delete("/delete_all")
+                .then()
+                .statusCode(200)
+                .body(is(expected))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("DELETE"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
+        given().when()
+                .delete("/delete_all")
+                .then()
+                .statusCode(200)
+                .body(is(""))
+                .header("content-type", is("text/plain"))
+                .header("Access-Control-Allow-Methods", is("DELETE"))
+                .header("Access-Control-Allow-Origin", is("*"))
+                .header("Access-Control-Allow-Headers", is("accept, content-type"));
     }
 
     @Test
